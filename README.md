32f2965b21122a35ed051240698ebf3e0bc24160
bradley <11279144+redSlug@users.noreply.github.com>
2025-01-16 12:27:54 -0500

    change crawling logic from recursive to iterative, add class and error handling to crawler, update README w/ setup instructions, update .gitignore for jetbrains

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # wiki-scraper-v2
+
+### Setup
+```
+pipenv install
+```
+
+## Run server
+```
+pipenv shell
+fastapi dev main.py
+```
+
--- scraper.py
+++ scraper.py
@@ -1,9 +1,23 @@
+from typing import List, Dict
+
 import httpx
-import re
 from urllib.parse import urlparse, urlunparse
 from bs4 import BeautifulSoup
+from dataclasses import dataclass
 
-def get_url(url, all_traveled_links=[]):
+all_traveled_links = list()
+already_visited = set()
+
+@dataclass
+class URLData:
+    links: List[Dict[str, str]]
+    base_url: str
+
+def construct_next_url(next, base="https://en.wikipedia.org/"):
+    return f"{base}{next}"
+
+def get_links_from_url(url: str) -> URLData:
+    print(f"Getting links from {url}")
     r = httpx.get(url)
 
     parsed_url = urlparse(url)
@@ -13,19 +27,48 @@ def get_url(url, all_traveled_links=[]):
     links = []
     for link in soup.css.select("#mw-content-text a"):
         links.append({"href": link.get("href"), "text" : link.text })
+    return URLData(links, next_base_url)
 
-    pattern = re.compile("^[a-z]")
+
+"""Main entry point
+must pass in URL, for example:
+https://en.wikipedia.org/wiki/Bear
+"""
+def get_url(url):
+    print(f"getting {url}")
+    result = get_links_from_url(url)
+    links = result.links
+    base_url = result.base_url
+    next_urls = list()
 
     for link in links:
-        if "/wiki/" in link['href'] and pattern.match(link['text']):
-            print("found match", link["href"], link["text"])
-            all_traveled_links.append({"href": link["href"], "text": link["text"]})
-            next_url = construct_next_url(next=link['href'], base=next_base_url)
-            get_url(next_url, all_traveled_links)
-            break
-        
-    return links
+        if 'href' not in link:
+            print('no href')
+            continue
 
+        if not link['href']:
+            print('href is falsey')
+            continue
 
-def construct_next_url(next, base="https://en.wikipedia.org/"):
-    return f"{base}{next}"
+        if link['href'] in already_visited:
+            print('already traveled')
+            continue
+
+        if not link['href'].startswith("/wiki/"):
+            print('not a wiki link')
+            continue
+
+        if not len(link['text']) > 0 or link['text'][0].isupper():
+            print('now a lower case text')
+            continue
+
+        print("found match", link["href"], link["text"])
+        already_visited.add(link["href"])
+        all_traveled_links.append(link)
+        next_url = construct_next_url(next=link['href'], base=base_url)
+        next_urls.append(next_url)
+        if not links:
+            links = next_urls
+            next_urls = list()
+    print(all_traveled_links)
+    return all_traveled_links
